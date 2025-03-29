b113236e00fc1be4f668b81af5d39ed5a7454b79
loordoza <131matmil131@gmail.com>
2025-03-29 21:32:34 +0100

    Add initial main menu

---
+++ Assets/Scripts/UI/WorldSaveGameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace ProjectPipe 
+{
+    public class WorldSaveGameManager : MonoBehaviour
+    {
+        [SerializeField] int worldSceneIndex = 1;
+
+        public static WorldSaveGameManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public IEnumerator LoadNewGame()
+        {
+            AsyncOperation loadOperation = SceneManager.LoadSceneAsync(worldSceneIndex);
+
+            yield return null;
+        }
+    }
+}
